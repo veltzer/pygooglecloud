61aae892050d3651025e0f334b24d1d0e1aa7407
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-23 12:54:19 +0300

    new version

--- pygooglecloud/static.py
+++ pygooglecloud/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.1"
+VERSION_STR = "0.0.2"
 DESCRIPTION = "pygooglecloud helps you with command line interaction with gcp"
 APP_NAME = "pygooglecloud"
 LOGGER_NAME = "pygooglecloud"
