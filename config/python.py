2292c256f97961c525ec69a0dc6f3aa413ca477b
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 10:10:12 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,32 +1,27 @@
-""" python depedencies for this project """
-from typing import List
+""" python deps for this project """
 
-
-console_scripts: List[str] = [
+console_scripts: list[str] = [
     "pygooglecloud=pygooglecloud.main:main",
 ]
-dev_requires: List[str] = [
-    "pypitools",
-    "black",
-]
-config_requires: List[str] = [
+
+config_requires: list[str] = [
     "pyclassifiers",
 ]
-install_requires: List[str] = [
+install_requires: list[str] = [
     "pylogconf",
     "pytconf",
     "google-auth",
 ]
-build_requires: List[str] = [
-    "pymakehelper",
+build_requires: list[str] = [
     "pydmt",
+    "pymakehelper",
 ]
-test_requires: List[str] = [
+test_requires: list[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
-    "flake8",
     "mypy",
+    # types
     "types-PyYAML",
 ]
 requires = config_requires + install_requires + build_requires + test_requires
