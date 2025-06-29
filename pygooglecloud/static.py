7e81df5a3a91c9a2377bb317ce95bfd71b80f857
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:43 +0300

    fixed up deps

--- pygooglecloud/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.3"
-DESCRIPTION = "pygooglecloud helps you with command line interaction with gcp"
-APP_NAME = "pygooglecloud"
-LOGGER_NAME = "pygooglecloud"
